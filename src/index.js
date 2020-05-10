b8901d0c78f782e8ada8108c9e15ace271bdb31e
Esdras Pavon <esdras.pavon@devlights.com>
2020-05-10 09:31:45 -0300

    Archivos y códigos innecesarios borrados

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React, { useContext } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "./routes/App";
 import "./styles/GlobalStyles.css";
 
-import Context from "./Context";
-ReactDOM.render(
-  <Context.Provider>
-    <App />
-  </Context.Provider>,
-  document.getElementById("app")
-);
+ReactDOM.render(<App />, document.getElementById("app"));
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,31 +1,16 @@
-import React, { useContext } from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
 // import NotFound from "../pages/NotFound";
-// import Layout from "../containers/Layout";
-// import PrivateRoute from "./PrivateRoute";
-
-import { Context } from "../Context";
 
 const App = () => {
-  const { isAuth, user } = useContext(Context);
   return (
     <BrowserRouter>
-      {/* <Layout> */}
       <Switch>
         <Route exact path="/" component={Home} />
-        {/* <PrivateRoute
-          exact
-          key="some-key"
-          admin
-          path="/url"
-          component={Component}
-          params={params_to_component}
-        /> */}
         {/* <Route component={NotFound} /> */}
       </Switch>
-      {/* </Layout> */}
     </BrowserRouter>
   );
 };
