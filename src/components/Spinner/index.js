7fb1c15c6686685dc10e8429e644ee384269ce40
Esdras Pavon <esdras.pavon@devlights.com>
2020-05-10 22:09:48 -0300

    Spinner agregado

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Loader } from './styles';
+
+const Spinner = () => (
+  <Loader>Cargando...</Loader>
+);
+
+export default Spinner;
---
+++ src/components/Spinner/styles.js
@@ -0,0 +1,33 @@
+import styled, { keyframes } from 'styled-components';
+
+const load = keyframes`
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);  
+  }
+`;
+
+export const Loader = styled.div`
+  border-radius: 50%;
+  width: 10em;
+  height: 10em;
+  margin: 60px auto;
+  font-size: 10px;
+  position: relative;
+  text-indent: -9999em;
+  border-top: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-right: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-bottom: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-left: 1.1em solid #ffffff;
+  transform: translateZ(0);
+  animation: ${load} 1.1s infinite linear;
+&:after {
+  border-radius: 50%;
+  width: 10em;
+  height: 10em;
+}
+`;
